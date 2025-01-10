ce82aa38f181127f25064b5083f0981adfe08afc
git branch -M main <git remote add origin https://github.com/nthm1806/tiktok-ui-H-a1862.git>
2025-01-11 02:26:04 +0700

    Hòa_11/1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import User from "./router/User";
+import Header from "./layout/components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header/>
+        <Routes>
+          <Route path="/user" element={<User />}/> {/* Sử dụng JSX */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
