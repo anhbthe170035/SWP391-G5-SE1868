dd3f81f63cbd3d272aab35d0d8166aac191dedea
git branch -M main <git remote add origin https://github.com/nthm1806/tiktok-ui-H-a1862.git>
2025-01-12 00:17:14 +0700

    Hòa_12/1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import User from "./router/User";
-import Header from "./layout/components/Header";
-import Transaction from "./router/Transaction";
 
 function App() {
   return (
     //Router, Routes, Route dùng để chuyển trang
-    <Router>
       <div>
-        <Header />
-        <Routes>
-          <Route path="/user" element={<User />} /> {/* Sử dụng JSX <User/> */}
-          <Route path="/transaction" element={<Transaction />} />
-          <Route path="/product" element={<Header />} />
-        </Routes>
+        hihi
       </div>
-    </Router>
   );
 }
 
